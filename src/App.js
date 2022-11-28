b7495d40ae3a94bee1fe10f57ff214795e67f1c4
ceciliecastor <ceciliecastor@gmail.com>
2022-11-28 13:22:33 +0100

    lille bitte rettelse

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import "./App.scss";
+import Price from "./components/price";
 
 function App() {
-  return <div className="App">
-    
-  </div>;
+  return <div className="App"></div>;
 }
 
 export default App;
