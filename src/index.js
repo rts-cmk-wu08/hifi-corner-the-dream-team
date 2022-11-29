94d897bf2bb9cb6f1d3509af9849613924236ae3
nadianielsen <U2404561@rts-365.dk>
2022-11-29 12:32:39 +0100

    fixed the router area in index

--- src/index.js
+++ src/index.js
@@ -42,8 +42,6 @@ export const router = createBrowserRouter(
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
-    <Header />
     <RouterProvider router={router} />
   </React.StrictMode>
 );
