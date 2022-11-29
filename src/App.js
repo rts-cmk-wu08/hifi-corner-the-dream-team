b6a623d6e71ba48eabc8a090daa9d2bfe10dbac9
nadianielsen <U2404561@rts-365.dk>
2022-11-29 10:50:50 +0100

    Er i gang med contactinfo form

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.scss";
 import Button from "./components/Button";
+import ContactInfo from "./components/ContactInfo";
 
 function App() {
   return <div className="App">
     <Button destination="/homepage" text="test"/>
-  </div>;
+    <ContactInfo />
+  </div>
+}
+
+export default App;
