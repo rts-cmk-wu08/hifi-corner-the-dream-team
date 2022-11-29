bd3181e0e06afb71a47298418c534fb9a8bdda21
nadianielsen <U2404561@rts-365.dk>
2022-11-29 13:02:21 +0100

    færdig med contact formularen

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import "./App.scss";
-import Button from "./components/Button";
 import ContactInfo from "./components/ContactInfo";
 import ProductImage from "./components/productImage";
 
 function App() {
   return (
     <div className="App">
-      <Button destination="/homepage" text="test" />
       <ProductImage />
       <ContactInfo />
     </div>
