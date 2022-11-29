1e32630e2c12ed8982a7c143e8ea3a3f7988d981
nadianielsen <U2404561@rts-365.dk>
2022-11-29 08:55:25 +0100

    et eller andet commit

--- src/index.js
+++ src/index.js
@@ -20,29 +20,21 @@ import Login from "./pages/Login";
 import InvoicePage from "./pages/InvoicePage";
 import NotFound from "./pages/NotFound";
 
-export const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<App />} />
-      <Route index element={<Homepage />} />
-      <Route path="/productpage" element={<Productpage />} />
-      <Route path="/productdetails" element={<ProductDetails />} />
-      <Route path="/moreinfo" element={<MoreInfo />} />
-      <Route path="/aboutus" element={<AboutUs />} />
-      <Route path="/CartPage" element={<CartPage />} />
-     <Route index element={<Homepage />} />
-     <Route path="/productpage" element={<Productpage />} />
-     <Route path="/productdetails" element={<ProductDetails />} />
-     <Route path="/moreinfo" element={<MoreInfo />} />
-     <Route path="/aboutus" element={<AboutUs />} />
-     <Route path="/cartpage" element={<CartPage />} />
-     <Route path="/paymentpage" element={<PaymentPage />} />
-     <Route path="/login" element={<Login />} />
-     <Route path="/invoice" element={<InvoicePage />} />
-     <Route path="/contactpage" element={<ContactPage />} />
-     <Route path='*' element={<NotFound />} />
-    </Route>
-  )
-);
+export const router = createBrowserRouter(createRoutesFromElements (
+  <Route path="/" element={<App />}>
+   <Route index element={<Homepage />} />
+   <Route path="/productpage" element={<Productpage />} />
+   <Route path="/productdetails" element={<ProductDetails />} />
+   <Route path="/moreinfo" element={<MoreInfo />} />
+   <Route path="/aboutus" element={<AboutUs />} />
+   <Route path="/cartpage" element={<CartPage />} />
+   <Route path="/paymentpage" element={<PaymentPage />} />
+   <Route path="/login" element={<Login />} />
+   <Route path="/invoice" element={<InvoicePage />} />
+   <Route path="/contactpage" element={<ContactPage />} />
+   <Route path='*' element={<NotFound />} />
+  </Route>
+))
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,6 +1,8 @@
 const ContactPage = () => {
     return ( 
-        <></>
+        <>
+        </>
+
      );
 }
  
