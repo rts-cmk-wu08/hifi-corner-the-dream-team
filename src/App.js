3e02f5e1f992a9c56c88bb23dc226c4d491e599e
ceciliecastor <ceciliecastor@gmail.com>
2022-11-29 10:42:06 +0100

    Flyttet produktbilleder ned i publicfolder Oprettet productImage

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import "./App.scss";
 import Button from "./components/Button";
+import ProductImage from "./components/productImage";
 
 function App() {
-  return <div className="App">
-    <Button destination="/homepage" text="test"/>
-  </div>;
+  return (
+    <div className="App">
+      <Button destination="/homepage" text="test" />
+      <ProductImage />
+    </div>
+  );
+}
+
+export default App;
