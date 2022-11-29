7ad1fdab5c8fccb968b832319898903ed1e758bd
ceciliecastor <ceciliecastor@gmail.com>
2022-11-29 11:18:16 +0100

    Oprettelse af invoice (på forkert branch)

---
+++ src/components/Invoice.js
@@ -0,0 +1,12 @@
+const Invoice = () => {
+  return (
+    <section className="invoice">
+      <div className="invoice__billing-adr">
+        <p className="invoice__name"></p>
+        <p className="invoice__name"></p>
+      </div>
+    </section>
+  );
+};
+
+export default Invoice;
