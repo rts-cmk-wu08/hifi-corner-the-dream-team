18005f02566a07d150608a1fe1638b50b3ce1355
ceciliecastor <ceciliecastor@gmail.com>
2022-12-01 12:08:11 +0100

    homepage scss små opdateringer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import "./App.scss";
 
 import ContactInfo from "./components/ContactInfo";
-import Invoice from "./components/Invoice";
-import Header from "./components/Header";
-import SearchBar from "./components/SearchBar";
-import Header from "./components/Header";
-import HomePage from "./pages/HomePage";
+import HomePage from "./pages/homepage";
 
 function App() {
   return (
     <div className="app">
-      <Invoice />
       <ContactInfo />
       <HomePage />
-      <SearchBar />
-      <Header />
-      </div>
-      )
-      }
-
+    </div>
+  );
+}
 
 export default App;
