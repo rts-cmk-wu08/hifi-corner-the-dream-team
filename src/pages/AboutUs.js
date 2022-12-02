f1e197ef69eb8779ba89708a5960a4fbdc99102d
Lukas Mikuta <lukasenokmikuta@gmail.com>
2022-12-02 10:26:09 +0100

    About Us er sat op, jeg fortsætter når vi har lavet api'et

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,6 +1,15 @@
 const AboutUs = () => {
     return ( 
-        <></>
+        <div className="divWrapper__grid">
+            {/* <h1 className="history__aboutus">our history</h1>
+
+            <section>
+                <img src= {fetch.billede} alt="" />
+                <h5> {fetch.faqheadline} </h5>
+                <h6 className="h6__subtitleAboutus"> {fetch.faqsubtitle} </h6>
+                <p> {fetch.faqtext} </p>
+            </section> */}
+        </div>
      );
 }
  
