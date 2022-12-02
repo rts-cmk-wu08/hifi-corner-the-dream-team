1288b36431046d63b8896fe8331f36439e198c7f
nadianielsen <U2404561@rts-365.dk>
2022-12-02 14:17:29 +0100

    scss har ændret

--- src/App.js
+++ src/App.js
@@ -2,15 +2,10 @@ import { Outlet } from "react-router-dom";
 import "./App.scss";
 import Header from "./components/Header";
 import SearchBar from "./components/SearchBar";
-import Footer from "./templates/Footer";
-import Header from "./components/Header";
-import SearchBar from "./components/SearchBar";
-import Video from "./components/Video";
 
 function App() {
   return (
     <div className="app">
-      <SearchBar />
       <Header />
       </div>
   );
