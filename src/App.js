1b21a25aecac7744cf1667e221c8d9dd3e3a7198
Lukas Mikuta <lukasenokmikuta@gmail.com>
2022-12-02 10:39:54 +0100

    Header og app rettet

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import "./App.scss";
 
 import ContactInfo from "./components/ContactInfo";
 import Invoice from "./components/Invoice";
-import Header from "./components/Header";
 import SearchBar from "./components/SearchBar";
 import Header from "./components/Header";
 import HomePage from "./pages/HomePage";
