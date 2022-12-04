19468050857d94b94a7d77d4301092c89d00fdca
ceciliecastor <ceciliecastor@gmail.com>
2022-12-04 18:43:53 +0100

    Er begyndt på productDetails

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 import { Outlet } from "react-router-dom";
 import "./App.scss";
-import Productpage from "./pages/ProductPage";
-import Footer from "./templates/Footer";
 import Header from "./components/Header";
-
 
 function App() {
   return (
     <div className="app">
-      <Productpage />
       <Header />
-      <Footer />
-      </div>
+      <Outlet />
+    </div>
   );
 }
 
--- src/pages/productpage.js
+++ src/pages/productpage.js
@@ -3,7 +3,7 @@ import ProductCard from "../templates/productCard";
 const Productpage = () => {
   return (
     <section className="product-page">
-      <h2 className="product-page__h2">products</h2>
+      <h1 className="product-page__h1">products</h1>
       <div className="product-page__2grid">
         <section className="product-page__sort">
           <div className="product-page__choice"></div>
