b0c41989455f923f2b0d4ccec125615390daedb5
nadianielsen <U2404561@rts-365.dk>
2022-12-05 09:00:15 +0100

    lavet component shopping cart og noget scss på den

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import "./App.scss";
-import Header from "./components/Header";
-import Footer from "./templates/Footer";
-import ContactPage from "./pages/ContactPage";
+import ShoppingCart from "./components/ShoppingCart";
 
 function App() {
   return (
     <div className="app">
-      <Header />
-      <ContactPage/>
-      <Footer className="footer" />
+      <ShoppingCart />
       </div>
   );
 }
