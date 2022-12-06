95c3030e35f8af2e382e9bc775ced13481917301
Lukas Mikuta <lukasenokmikuta@gmail.com>
2022-12-06 12:44:36 +0100

    Søgefunktionen er færdig

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,5 +1,7 @@
 import { useState } from "react";
-import "./styles.css";
+import "./SearchBar";
+import {BrowserRouter as Router, Link} from "react-router-dom"
+import { FaSearch } from 'react-icons/fa';
 var data = require("./MOCK_DATA.json");
 
 export default function Search() {
@@ -17,30 +19,34 @@ export default function Search() {
 
   return (
     <div className="App">
-      <h1>Search</h1>
     
       <div className="search-container">
         <div className="search-inner">
-          <input type="text" value={value} onChange={onChange} />
-          <button onClick={() => onSearch(value)}> Search </button>
-        </div>
-        <div className="dropdown">
+          <input placeholder="Search product..." type="text" value={value} onChange={onChange} className="input__search" />
+          <div className="fontuser">
+        <Router>
+      <Link to={"/Tets"}>
+      <FaSearch color="000000" />
+      </Link>
+        </Router>
+          </div>
+          </div>
+    <div className="dropdown">
           {data
             .filter((item) => {
-              const searchTerm = value.toLowerCase();
-              const searchProducts = item.search_products.toLowerCase();
-              
-              return (
-                searchTerm &&
-                searchProducts.startsWith(searchTerm) &&
-                searchProducts !== searchTerm
-              );
-            })
-            .slice(0, 10)
-            .map((item) => (
-              
-           <div onClick={() => onSearch(item.search_products)} className="dropdown-row" key={item.search_products} > {item.search_products} </div>
-            ))}
+                const searchTerm = value.toLowerCase();
+                const searchProducts = item.search_products.toLowerCase();
+                
+                return (
+                    searchTerm &&
+                    searchProducts.startsWith(searchTerm) &&
+                    searchProducts !== searchTerm
+                    );
+                })
+                .slice(0, 10)
+                .map((item) => (
+                    <div onClick={() => onSearch(item.search_products)} className="dropdown-row" key={item.search_products} > {item.search_products} </div>
+                    ))}
         </div>
       </div>
     </div>
