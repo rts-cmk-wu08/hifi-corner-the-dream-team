37c6e25ce17b43de5746f090570a254e860eb2a0
nadianielsen <U2404561@rts-365.dk>
2022-12-06 10:29:24 +0100

    konflikter!!!!

--- src/components/CountItems.js
+++ src/components/CountItems.js
@@ -1,25 +1,21 @@
-import { useState, useContext } from "react";
+import { useContext } from "react";
 import { DestinationContext } from "../contexts/DestinationContext";
 
 
 const CountItems = () => {
-    const [itemCount, setItemCount] = useState(0);
-
-    const handleIncrease = () => itemCount < 10 && setItemCount(itemCount + 1)
-    const handleDecrease = () => itemCount > 0 && setItemCount(itemCount - 1)
-
     const { updateBookings} = useContext(DestinationContext)
 
     const handleClick = () => {
         updateBookings()
     }
 
+    
+
     return ( 
         <div className="flex__countitems">
         <button className="minus" onClick={handleDecrease}>-</button>
-        <div className="count__box">{itemCount}</div>
-        <button className="plus" onClick={handleIncrease}>+</button>
-        <button onClick={handleClick}>Book this destination</button>
+        <div className="count__box">{handleClick}</div>
+        <button className="plus" onClick={ handleClick }>+</button>
         </div>
      );
 }
--- src/contexts/DestinationContext.js
+++ src/contexts/DestinationContext.js
@@ -6,10 +6,17 @@ const DestinatioProvider = (props) => {
 
     const [bookings, setBookings] = useState(0);
 
+    const [itemCount, setItemCount] = useState(0);
+
+    const handleIncrease = () => itemCount < 10 && setItemCount(itemCount + 1)
+    const handleDecrease = () => itemCount > 0 && setItemCount(itemCount - 1)
+
     const updateBookings = () => {
         setBookings(bookings + 1)
+        handleIncrease()
+        itemCount()
     }
-
+    
     return ( 
         <DestinationContext.Provider value= {{ bookings, setBookings, updateBookings }}>
             {props.children}
