6b72e8f7b6fa9968ee76563643c141a94c866b76
ceciliecastor <ceciliecastor@gmail.com>
2022-12-08 13:23:08 +0100

    Små rettelser i filnavne (...igen)

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,10 +1,10 @@
-import ContactInfo from "../components/ContactInfo";
+import ContactForm from "../components/ContactInfo";
 
 const ContactPage = () => {
   return (
     <section className="contactinfo">
       <h1 className="contactinfo__heading">Get in touch with us</h1>
-      <ContactInfo />
+      <ContactForm />
       <h3 className="contactinfo__h3">
         Visit our sister companies{" "}
         <span className="contactinfo__h3--orange">Home Sound</span> and{" "}
