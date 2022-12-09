484ac87aecd36abeb6e953ec3bb650a3ae04a196
nadianielsen <U2404561@rts-365.dk>
2022-12-09 12:18:55 +0100

    rettede nogle ting

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ function App() {
   return (
     <div className="app">
       <Header />
-      <Footer className="footer" />
       <Outlet />
+      <Footer className="footer" />
       </div>
   );
 }
