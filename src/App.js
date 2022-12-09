a842eaed62be3a8ec5b27d5dc3a83c41ca104ec9
nadianielsen <U2404561@rts-365.dk>
2022-12-09 13:37:24 +0100

    fikset fejl

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.scss";
 import Header from "./components/Header";
 import Footer from "./templates/Footer";
+import { Outlet } from "react-router-dom";
 
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Header />
       <Outlet />
       <Footer />
+      </div>
   );
 }
 
