368a1b5d3fdb9f2ee518a2831c739e8dbcd39dfc
nadianielsen <U2404561@rts-365.dk>
2022-12-09 11:13:35 +0100

    productpage - sætter ting ind fra apiet

--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -1,13 +1,22 @@
 import ProductCard from "../templates/productCard";
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
+// import axios from "axios";
 
 const HomePage = () => {
 
-
+  // const [loading, setLoading] = useState(true);
+  // const [error, setError] = useState();
+  // const [productsHomePage, setProductsHomePage] = useState();
+  
+  // useEffect(() => {
+  //   axios("http://localhost:4000/products")
+  //   .then(response => setProductsHomePage(response.data))
+  //   .catch(() => setError("Oops!!, something went wrong"))
+  //   .finally(() => setLoading(false))
+  // }, []);
   
 
   return (
-
     <article className="homepage">
     <video autoPlay muted className="hero__video" src="hero_video.mp4"></video>
       <h2 className="homepage__h2">popular products</h2>
--- src/templates/productCard.js
+++ src/templates/productCard.js
@@ -1,17 +1,38 @@
 import Button from "../components/Button";
 import Price from "../components/price";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 
 const ProductCard = (props) => {
+
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState();
+  const [products, setProducts] = useState();
+  
+  useEffect(() => {
+    axios("http://localhost:4000/products?_limit=4")
+    .then(response => setProducts(response.data))
+    .catch(() => setError("Oops!!, something went wrong"))
+    .finally(() => setLoading(false))
+  }, []);
+
   return (
-    <section className="product-card">
-      <img className="product-card__img" src={props.src} alt={props.alt} />
+    <div>
+      {loading && <p>Loading...</p>}
+      {error && <p>{error}</p>}
+      {products && products.map(product => (
+        <section className="product-card">
+      <img className="product-card__img" src={product.image.url} alt={props.alt} />
       <div className="product-card__txt">
-        <h3 className="product-card__h3">Dynamisk titel {props.titel}</h3>
-        <h3 className="product-card__h3">Dynamisk titel {props.output}</h3>
+          <h3 className="product-card__h3">{product.name}</h3>
+          <p>{product.general.dimensions}</p>
       </div>
-      <Price className="product-card__price" price="pris" />
+          <Price className="product-card__price" price={product.price + ".00"} />
       <Button destination="/productdetails" text="Read more" />
     </section>
+          ))}
+    </div>
   );
 };
 
