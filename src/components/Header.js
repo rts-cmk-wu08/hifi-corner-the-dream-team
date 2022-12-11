a1deb593afda6a26bac6d87f60a0c8ab2c68aef1
ceciliecastor <ceciliecastor@gmail.com>
2022-12-11 22:14:15 +0100

    arbejdet på headeren

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
-import { BsFillPersonFill } from "react-icons/bs";
+import { FaShoppingCart } from "react-icons/fa";
 import SearchBar from "./SearchBar";
 
 const Header = () => {
@@ -15,35 +15,82 @@ const Header = () => {
   };
 
   return (
-    <header>
-        <nav className="headerNavbar">
-        <Link className="text__decoration" to={"/"}><img className="hif1cornerlogo" src="hifi-corner-logo-01.svg" alt="hifi corner logo" /></Link>
-          <li onMouseOver={handleMouseOver} onMouseOut={handleMouseOut} className="navbar__onLine">shop</li>
-          <Link to={"/AboutUs"} className="text__decoration"><li className="navbar__onLine">about us</li></Link>
-          <Link to={"/ContactUs"} className="text__decoration"><li className="navbar__onLine">contact us</li></Link>
-        </nav>
-
-          <li
-            className="shopElmNav"
-            onMouseOver={handleMouseOver}
-            onMouseOut={handleMouseOut}
-          >
-          </li>
-          
-        <div className="div__shopList" onMouseOver={handleMouseOver} onMouseOut={handleMouseOut}>
-        {isHovering && <p className="browseShopList">Browse Categories</p>}
-        <Link to= {"/CDPlayers"} className="text__decoration"> {isHovering && <p className="shopList">CD Players</p>} </Link>
-        <Link to= {"/CDPlayers"} className="text__decoration"> {isHovering && <p className="shopList">DVD Players</p>} </Link>
-        <Link to= {"/Preamps"} className="text__decoration"> {isHovering && <p className="shopList">Preamps</p>} </Link>
-        <Link to= {"/Speakers"} className="text__decoration"> {isHovering && <p className="shopList">Speakers</p>} </Link>
-        <Link to= {"/Turntabels"} className="text__decoration"> {isHovering && <p className="shopList">Turntabels</p>} </Link>
-        <Link to= {"/IntegratedAmplifiers"} className="text__decoration"> {isHovering && <p className="shopList">Integrated Amplifiers</p>} </Link>
-        <Link to= {"/PowerAmplifiers"} className="text__decoration"> {isHovering && <p className="shopList">Power Amplifiers</p>} </Link>
-        <Link to= {"/TubeAmplifiers"} className="text__decoration"> {isHovering && <p className="shopList">Tube Amplifiers</p>} </Link>
+    <header className="header">
+      <nav className="header__navbar">
+        <Link className="text__decoration" to={"/"}>
+          <img
+            className="hif1cornerlogo"
+            src="hifi-corner-logo-01.svg"
+            alt="hifi corner logo"
+          />
+        </Link>
+        <li
+          onMouseOver={handleMouseOver}
+          onMouseOut={handleMouseOut}
+          className="navbar__onLine"
+        >
+          shop
+        </li>
+        <Link to={"/AboutUs"} className="text__decoration">
+          <li className="navbar__onLine">about us</li>
+        </Link>
+        <Link to={"/ContactUs"} className="text__decoration">
+          <li className="navbar__onLine">contact us</li>
+        </Link>
+        <li
+          className="shopElmNav"
+          onMouseOver={handleMouseOver}
+          onMouseOut={handleMouseOut}
+        ></li>
+        <div
+          className="div__shopList"
+          onMouseOver={handleMouseOver}
+          onMouseOut={handleMouseOut}
+        >
+          {isHovering && <p className="browseShopList">Browse Categories</p>}
+          <Link to={"/CDPlayers"} className="text__decoration">
+            {" "}
+            {isHovering && <p className="shopList">CD Players</p>}{" "}
+          </Link>
+          <Link to={"/CDPlayers"} className="text__decoration">
+            {" "}
+            {isHovering && <p className="shopList">DVD Players</p>}{" "}
+          </Link>
+          <Link to={"/Preamps"} className="text__decoration">
+            {" "}
+            {isHovering && <p className="shopList">Preamps</p>}{" "}
+          </Link>
+          <Link to={"/Speakers"} className="text__decoration">
+            {" "}
+            {isHovering && <p className="shopList">Speakers</p>}{" "}
+          </Link>
+          <Link to={"/Turntabels"} className="text__decoration">
+            {" "}
+            {isHovering && <p className="shopList">Turntabels</p>}{" "}
+          </Link>
+          <Link to={"/IntegratedAmplifiers"} className="text__decoration">
+            {" "}
+            {isHovering && (
+              <p className="shopList">Integrated Amplifiers</p>
+            )}{" "}
+          </Link>
+          <Link to={"/PowerAmplifiers"} className="text__decoration">
+            {" "}
+            {isHovering && <p className="shopList">Power Amplifiers</p>}{" "}
+          </Link>
+          <Link to={"/TubeAmplifiers"} className="text__decoration">
+            {" "}
+            {isHovering && <p className="shopList">Tube Amplifiers</p>}{" "}
+          </Link>
         </div>
-  </header>
- 
-
+      </nav>
+      <div className="header__right">
+        <SearchBar />
+        <div className="header__icons">
+          <FaShoppingCart className="header__icon" />
+        </div>
+      </div>
+    </header>
   );
 };
 
