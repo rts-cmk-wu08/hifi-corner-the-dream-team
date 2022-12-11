eeb5f1c34dd39c7071d68c3d7046c0da44270b3d
ceciliecastor <ceciliecastor@gmail.com>
2022-12-11 19:45:57 +0100

    Fixed the footer contact info and placement

--- src/components/LiLinkFooter.js
+++ src/components/LiLinkFooter.js
@@ -1,9 +1,13 @@
 import { Link } from "react-router-dom";
 
 const LiLinkFooter = (props) => {
-    return ( 
-        <li className="padding__top"><Link className="footer__link" to={props.destination}>{props.text}</Link></li>
-     );
-}
- 
+  return (
+    <li>
+      <Link className="footer__link" to={props.destination}>
+        {props.text}
+      </Link>
+    </li>
+  );
+};
+
 export default LiLinkFooter;
