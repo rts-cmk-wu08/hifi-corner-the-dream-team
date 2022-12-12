517def4865bce77cf0275a4149afc5c1a2260e94
nadianielsen <U2404561@rts-365.dk>
2022-12-12 08:35:43 +0100

    rydder op

--- src/App.js
+++ src/App.js
@@ -2,26 +2,20 @@ import { Outlet } from "react-router-dom";
 import "./App.scss";
 import Footer from "./templates/Footer";
 import Header from "./components/Header";
-import SearchBar from "./components/SearchBar";
 import Video from "./components/Video";
 import HomePage from "./pages/HomePage";
-import CartCard from "./templates/cartCard";
-import ContactInfo from "./components/ContactInfo";
-import HomePage from "./pages/homepage";
+// import CartCard from "./templates/cartCard";
 
 function App() {
   return (
     <div className="app">
-      <ContactInfo />
-      <HomePage />
-      <SearchBar />
       <Header />
       <Video />
+      <HomePage />
+      <Outlet />
       <Footer />
       
-      <Outlet />
       </div>
-     </div>
   );
 }
 
