9f9738350fe68e3e77400b77997e572d198ae752
MatasMot <98148619+MatasMot@users.noreply.github.com>
2022-12-12 14:19:50 +0100

    some HTML and buttons

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 import "./App.scss";
 import Footer from "./templates/Footer";
 import Header from "./components/Header";
-import DeliveryMethods from "./templates/deliveryMethods";
+import DeliveryMethods from "./templates/DeliveryMethods";
 
 
 function App() {
@@ -11,7 +11,7 @@ function App() {
       <Header />
       <main className="main">
         <Outlet />
-        <DeliveryMethod/>
+        <DeliveryMethods/>
       </main>
       <Footer />
     </div>
--- src/templates/DeliveryMethods.js
+++ src/templates/DeliveryMethods.js
@@ -1,28 +1,44 @@
 
 const DeliveryMethods = () => {
     return ( 
-        <section>
-            Home devilery
-            Click-and-collect
-            Postoffice
+        <section className="deliveryMethods">
 
-            <h2>Your order will be shipped to</h2>
+            <h2 className="deliveryMethods__h2">Select your prefered delivery method</h2>
+            <div className="deliveryMethods__methods">
+                <div className="deliveryMethods__buttons">
+                    <button>Home delivery</button>
+                    <button>Click-and-collect</button>
+                    <button>Postoffice</button>
+                </div>
+            
+                <h3 className="deliveryMethods__h3">Your order will be shipped to</h3>
+                
+                <p>61 Church St</p>
+                <p>Berwick-upon-Tweed</p>
+                <p>Northumberland</p>
+                <p>TD15 1EE</p>
+                <p>United Kingdom</p>
 
-            <h3>Edinburgh</h3>
-            <p>2 Joppa Rd,Edinburgh, EH15 2EU</p>
-            <p>Monday to Friday: 10:00am - 5:30pm</p>
-            <p>Saturday: 10:00am - 5:30pm</p>
-            <p>Sunday: Closed</p>
+                <input type="radio" name="EdinburghOrFalkirk" className="deliveryMethods_radio-edinburgh" />
+                <label htmlFor="EdinburghOrFalkirk">
+                    <h4 className="deliveryMethods__h4">Edinburgh</h4>
+                </label>
+                <p>2 Joppa Rd,Edinburgh, EH15 2EU</p>
+                <p>Monday to Friday: 10:00am - 5:30pm</p>
+                <p>Saturday: 10:00am - 5:30pm</p>
+                <p>Sunday: Closed</p>
 
-            <h3>Falkirk</h3>
-            <p>44 Cow Wynd, Falkirk, Central Region, FK1 1PU</p>
-            <p>Monday to Friday: 10:00am - 5:30pm</p>
-            <p>Saturday - By appointment only</p>
-            <p>Sunday: Closed</p>
-
+                <input type="radio" name="EdinburghOrFalkirk" className="deliveryMethods_radio-lalkirk" />
+                <label htmlFor="EdinburghOrFalkirk">
+                    <h4 className="deliveryMethods__h4">Falkirk</h4>
+                </label>
+                <p>44 Cow Wynd, Falkirk, Central Region, FK1 1PU</p>
+                <p>Monday to Friday: 10:00am - 5:30pm</p>
+                <p>Saturday - By appointment only</p>
+                <p>Sunday: Closed</p>
+            </div>
         </section>
-        
      );
     }
- 
+
 export default DeliveryMethods;
