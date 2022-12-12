28870b3208c06f36dde9f7de1ffbecdbec96ad30
MatasMot <98148619+MatasMot@users.noreply.github.com>
2022-12-12 12:25:47 +0100

    DeliveryMethods .js and .scss files created

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Outlet } from "react-router-dom";
 import "./App.scss";
 import Footer from "./templates/Footer";
 import Header from "./components/Header";
+import DeliveryMethods from "./templates/deliveryMethods";
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Header />
       <main className="main">
         <Outlet />
+        <DeliveryMethod/>
       </main>
       <Footer />
     </div>
---
+++ src/templates/DeliveryMethods.js
@@ -0,0 +1,28 @@
+
+const DeliveryMethods = () => {
+    return ( 
+        <section>
+            Home devilery
+            Click-and-collect
+            Postoffice
+
+            <h2>Your order will be shipped to</h2>
+
+            <h3>Edinburgh</h3>
+            <p>2 Joppa Rd,Edinburgh, EH15 2EU</p>
+            <p>Monday to Friday: 10:00am - 5:30pm</p>
+            <p>Saturday: 10:00am - 5:30pm</p>
+            <p>Sunday: Closed</p>
+
+            <h3>Falkirk</h3>
+            <p>44 Cow Wynd, Falkirk, Central Region, FK1 1PU</p>
+            <p>Monday to Friday: 10:00am - 5:30pm</p>
+            <p>Saturday - By appointment only</p>
+            <p>Sunday: Closed</p>
+
+        </section>
+        
+     );
+    }
+ 
+export default DeliveryMethods;
