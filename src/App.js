76253f1c95a1ddccf0d1bf1f71e8cec2f0b458dd
nadianielsen <U2404561@rts-365.dk>
2022-12-12 08:28:57 +0100

    prøver at style footer bedre

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 import "./App.scss";
 import Footer from "./templates/Footer";
 import Header from "./components/Header";
-import ContactFormTwo from "./formComponents/contactForm";
+// import ContactFormTwo from "./formComponents/contactForm";
 
 
 
