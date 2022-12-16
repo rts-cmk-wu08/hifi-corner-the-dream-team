292197cb07c41c7e0daa42ce4e236235d7bab994
ceciliecastor <ceciliecastor@gmail.com>
2022-12-16 08:57:22 +0100

    små rettelser

--- src/App.js
+++ src/App.js
@@ -1,17 +1,7 @@
 import { Outlet } from "react-router-dom";
 import "./App.scss";
-// import ContactInfo from "./components/ContactInfo";
-// import Invoice from "./components/Invoice";
-// import SearchBar from "./components/SearchBar";
-// import ImageSlider from "./templates/ImageSlider";
-// import { SliderData } from "./templates/SliderData";
 import Footer from "./templates/Footer";
-// import HomePage from "./pages/HomePage";
-// import CartCard from "./templates/cartCard";
-// import PaymentPageForm from "./templates/paymentPageForm";
 import Header from "./components/Header";
-//import CartContextProvider from "./context/cartContext";
-
 
 function App() {
   return (
