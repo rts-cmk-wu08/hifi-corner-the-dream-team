b21332aac91f0c08b4d597d38da6cfa540597a4f
ceciliecastor <ceciliecastor@gmail.com>
2022-12-20 08:15:46 +0100

    Små rettelser i form validering

--- src/pages/CreateAccount.js
+++ src/pages/CreateAccount.js
@@ -2,9 +2,10 @@ import NewCustomerForm from "../formComponents/newCustomerForm";
 
 const CreateAccount = () => {
   return (
-    <>
+    <section className="create-account">
+      <h1 className="create-account__h1">Create an account</h1>
       <NewCustomerForm />
-    </>
+    </section>
   );
 };
 
