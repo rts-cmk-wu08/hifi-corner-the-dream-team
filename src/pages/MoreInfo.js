0e873f4fc774a971d794973087f49788ea4c4547
ceciliecastor <ceciliecastor@gmail.com>
2022-12-21 09:05:18 +0100

    Kom til at slette det forkerte component Er rettet igen

---
+++ src/pages/MoreInfo.js
@@ -0,0 +1,12 @@
+import Faq from "../components/faq";
+
+const MoreInfo = () => {
+  return (
+    <section className="faq">
+      <h1 className="faq__h1">FAQ</h1>
+      <Faq />
+    </section>
+  );
+};
+
+export default MoreInfo;
